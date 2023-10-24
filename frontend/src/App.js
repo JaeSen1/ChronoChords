032e40b6d091542393a739e57dd1a5d5a4f121fc
jbryan75 <jbryan75@uncc.edu>
2023-10-23 20:41:22 -0400

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Appbar from './components/Appbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Appbar.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Appbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ backgroundColor: '#1B1B1B' }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontFamily: 'Monomaniac One, sans-serif', fontSize: 64}}>
+            ChronoChords
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
