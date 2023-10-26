53ae7212c98e9a6418d9228e03a300eb4234567d
Vedi <Vgharibi@uncc.edu>
2023-10-26 18:43:53 -0400

    Build: Added more Backend Structure Including Controller-Model-Repository

--- backend/src/main/java/com/ChronoChords/ChronoChords/model/user.java
+++ backend/src/main/java/com/ChronoChords/ChronoChords/model/user.java
@@ -32,6 +32,22 @@ public class user {
     public void setId(long id) {
         this.id = id;
     }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
 }
 
 
