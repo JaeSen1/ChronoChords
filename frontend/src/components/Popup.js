950c7ed97ef3a8250c89caf426e61c1eb0f9534b
Alvin Li <ali13@uncc.edu>
2023-11-14 13:31:56 -0500

    added tabs to pop up

--- frontend/src/components/Popup.js
+++ frontend/src/components/Popup.js
@@ -3,50 +3,86 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 const style = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: '70%', // or choose a size that fits your needs
-  maxWidth: '600px', // Avoid the modal getting too large on big screens
+  width: '70%',
+  maxWidth: '1000px',
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  border: '1px solid #000',
   boxShadow: 24,
   p: 4,
-  overflowY: 'auto', // Ensures the modal scrolls if content is too big
+  overflowY: 'auto',
 };
 
-export default function BasicModal({ open, onClose, songData }) {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
   return (
-    <div>
-      <Modal
-        open={open}
-        onClose={onClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Button style={{ position:'absolute', top: '10px', right: '10px' }} onClick={onClose}>Close</Button>
-
-          {/* Song title and artist */}
-          <Typography id="modal-modal-title" variant="h6" component="h2" marginBottom="2">
-            {songData.title} - {songData.artist}
-          </Typography>
-
-          {/* Album name */}
-          <Typography variant="subtitle1" color="text.secondary" marginBottom="2">
-            {songData.album}
-          </Typography>
-
-          {/* Description section */}
-          <Typography id="modal-modal-description" sx={{ mt: 2, wordWrap: 'break-word' }}>
-            {songData.artistDescription} {/* Placeholder for real description */}
-          </Typography>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
         </Box>
-      </Modal>
+      )}
     </div>
+  );
+}
+
+export default function BasicModal({ open, onClose, songData }) {
+  const [tabValue, setTabValue] = React.useState(0);
+
+  const handleTabChange = (event, newValue) => {
+    setTabValue(newValue);
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Button style={{ position: 'absolute', top: '10px', right: '10px' }} onClick={onClose}>Close</Button>
+        <Typography id="modal-modal-title" variant="h6" component="h2" marginBottom="2">
+          {songData.title} - {songData.artist}
+        </Typography>
+        <Typography variant="subtitle1" color="text.secondary" marginBottom="2">
+          {songData.album}
+        </Typography>
+
+        {/* Tabs */}
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={tabValue} onChange={handleTabChange} aria-label="basic tabs example">
+            <Tab label="Artist" />
+            <Tab label="Song" />
+            <Tab label="Year" />
+          </Tabs>
+        </Box>
+
+        {/* Tab Panels */}
+        <TabPanel value={tabValue} index={0}>
+          {songData.artistDescr}
+        </TabPanel>
+        <TabPanel value={tabValue} index={1}>
+          {songData.description}
+        </TabPanel>
+        <TabPanel value={tabValue} index={2}>
+          {songData.aboutYear}
+        </TabPanel>
+      </Box>
+    </Modal>
   );
 }
